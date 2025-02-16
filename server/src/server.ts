d0b6faf54c341959ec625f1eb04f12be7dad6642
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-02-16 15:56:47 -0600

    moved placement of import routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 import express, { urlencoded } from 'express';
-import routes from './routes/index.js';
 dotenv.config();
 
 // Import the routes
+import routes from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
