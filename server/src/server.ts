53dc53c7426132029dc5480eea409395f6524fd3
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-02-16 15:05:20 -0600

    updates to server.ts with middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 dotenv.config();
 
 // Import the routes
@@ -10,11 +11,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname,'client/dist')));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
+
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
