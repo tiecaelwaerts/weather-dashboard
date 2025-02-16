8b72323903ce568041ac253d672f574cc99ec49a
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-02-16 15:40:19 -0600

    updates to server, remove path import

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,22 @@
 import dotenv from 'dotenv';
-import express from 'express';
-import path from 'path';
+import express, { urlencoded } from 'express';
+import routes from './routes/index.js';
 dotenv.config();
 
 // Import the routes
-import routes from './routes/index.js';
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname,'client/dist')));
+app.use(express.static('../client/dist'));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes
-app.use('/api', routes);
+app.use(routes);
 
 
 // Start the server on the port
