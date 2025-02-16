b6af1fe3cc5964014628c929b5de984f60226eb0
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-02-16 15:31:16 -0600

    updated middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes
-app.use(routes);
+app.use('/api', routes);
 
 
 // Start the server on the port
