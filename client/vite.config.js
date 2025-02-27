d2a92746cdefc7f2362899070930d95d3813bf39
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-02-26 21:26:55 -0600

    fixes to config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
     open: true,
