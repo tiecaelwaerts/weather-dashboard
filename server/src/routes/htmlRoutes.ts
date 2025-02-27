21ae500ff72454f8fc8a1294f7536c7b213d461c
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-02-26 22:15:57 -0600

    updates to routes and server

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,14 +1,14 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 const router = Router();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Define route to serve index.html
-router.get('/', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+router.get('*', (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,5 @@
 import dotenv from 'dotenv';
 import express, { urlencoded } from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 dotenv.config();
 
 // Import the routes
@@ -10,12 +8,8 @@ import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Create __dirname equivalent for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Serve static files of the client dist folder
-app.use(express.static(path.join(__dirname, '../../../client/dist')));
+app.use(express.static('../client/dist'));
 
 // Implement middleware for parsing JSON and urlencoded form datadleware for parsing JSON and urlencoded form data
 app.use(express.json());
