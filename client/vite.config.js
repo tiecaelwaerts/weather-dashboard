f3b49ef57dcbc4f03d5a0c24dbc7c6e86d1935b5
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-02-26 22:42:32 -0600

    remove vite.config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,6 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-  },
   server: {
     port: 3000,
     open: true,
